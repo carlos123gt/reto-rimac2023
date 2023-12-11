f5d2f02652df90cb723928ffe2256dc34e3c5291
daniel123gt <dailgozalez@gmail.com>
2023-12-11 06:50:31 -0500

    deploy run

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://daniel123gt.github.io/reto-rimac2023/'
+  base: '/reto-rimac2023/'
 })
